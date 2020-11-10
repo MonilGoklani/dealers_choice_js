9f79b5d760ccc222fc25ced459d1377830405157
MonilGoklani <monil2912@gmail.com>
2020-11-10 18:42:27 -0500

    final solution with db

--- topTenMovies.js
+++ topTenMovies.js
@@ -4,22 +4,28 @@ const movieInfo = require('./views/movieInfo');
 const path = require('path');
 const movieData = require('./movieData');
 const app = express();
+const {client,syncAndSeed} = require('./db')
 
 console.log(path.join(__dirname,'public','thumbails'))
 app.use(express.static(path.join(__dirname,'public','thumbnails')))
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/',(req,res,next)=>{
-res.send(movieList(movieData.list()))
+app.get('/', async(req,res,next)=>{
+    try{
+        const movielist = await client.query(`SELECT * FROM movies;`)
+        res.send(movieList(movielist.rows))
+    }catch(error){
+        next(error)
+    }
 })
 
-app.get('/details/:id',(req,res,next)=>{
-    const id = req.params.id
-    const movie = movieData.find(id)
-    if(!movie.id){
+app.get('/details/:id',async(req,res,next)=>{
+    const movie = await client.query(`SELECT * FROM movies WHERE movies.id=${req.params.id};`)
+    console.log(movie.rows[0])
+    if(!movie.rows[0].id){
         throw new Error('Not Found')
     }
-    res.send(movieInfo(movie))
+    res.send(movieInfo(movie.rows[0]))
     })
 
 const port = process.env.PORT || 3000
@@ -30,3 +36,14 @@ app.use(function(err,req,res,next){
 })
 
 app.listen(port,()=>{console.log(`Listening on port: ${port}`)})
+
+const init = async()=>{
+    try{
+        await client.connect();
+        syncAndSeed()
+    }catch(error){
+        console.log(error)
+    }
+}
+
+init();
